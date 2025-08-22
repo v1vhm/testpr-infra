d8dee4af1cdad2fcc0b0225932cb22abf352d45c
mattv1vhm <130549829+mattv1vhm@users.noreply.github.com>
2025-08-22 19:30:58 +0000

    Fix resource group reference in module configuration

--- main.tf
+++ main.tf
@@ -12,5 +12,5 @@ module "avm-ptn-aiml-ai-foundry" {
 
   location                   = "uksouth"
   base_name                  = "v1vhm-ai"
-  resource_group_resource_id = azurerm_resource_group.this.id
+  resource_group_resource_id = data.azurerm_resource_group.this.id
 }
