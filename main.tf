13c88ee5d9017f726e421496d125a15d75580ab8
mattv1vhm <130549829+mattv1vhm@users.noreply.github.com>
2025-08-22 21:38:35 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,5 +7,5 @@ module "avm-ptn-aiml-ai-foundry" {
 
   location                   = "uksouth"
   base_name                  = "v1vhm-ai"
-  resource_group_resource_id = data.azurerm_resource_group.this.id
+  resource_group_resource_id = data.azurerm_resource_group.deployment-rg.id
 }
