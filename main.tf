ab6d1fe5f67a90bcee2e1b17af21170ad7870a24
mattv1vhm <130549829+mattv1vhm@users.noreply.github.com>
2025-08-22 20:15:02 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
 # Main Terraform configuration
 
 # TODO: Add resources and module invocations here.
+
+module "avm-ptn-function-app-storage-private-endpoints" {
+  source  = "Azure/avm-ptn-function-app-storage-private-endpoints/azurerm"
+  version = "0.2.0"
+  
+  name = "v1vhm-testpr-fnapp"
+  resource_group_name = "rg-TESTPR-dev-uksouth"
+  os_type = "linux"
+
+}
